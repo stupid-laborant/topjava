32fd00889e90b2fccec8d74563d46e48308796e5
qqq <qqq@mail.com>
2021-07-07 18:09:13 +0200

    populate script implemented config issue resolved MealServiceTest is created

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,6 @@
 DELETE FROM user_roles;
 DELETE FROM users;
+DELETE FROM meals;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password)
@@ -9,3 +10,11 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+INSERT INTO meals (date_time, user_id, description, calories)
+VALUES (timestamp '2021-06-30 10:00:00', 100000, 'Завтрак', '500'),
+       (timestamp '2021-06-30 13:00:00', 100000, 'Обед', '1000'),
+       (timestamp '2021-06-30 20:00:00', 100000, 'Ужин', '500'),
+       (timestamp '2021-07-01 00:00:00', 100000, 'Еда на граничное значение', '100'),
+       (timestamp '2021-07-01 10:00:00', 100000, 'Завтрак', '1000'),
+       (timestamp '2021-07-01 13:00:00', 100000, 'Обед', '500'),
+       (timestamp '2021-07-01 20:00:00', 100000, 'Ужин', '410');
